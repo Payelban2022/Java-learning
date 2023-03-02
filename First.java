348883c8838ad29fa2f46484efc4766777815f88
Payel <payel.nandy2580@gmail.com>
2023-03-03 00:05:19 +0530

    Learning

---
+++ First.java
@@ -0,0 +1,7 @@
+public class First {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        String var = "Payel \u00BB";
+        System.out.println("Name: "+ var);
+    }
+}
