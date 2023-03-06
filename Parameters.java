60c9e53148f9a3167ae184c4d2f9b24ebe678a40
Payel <payel.nandy2580@gmail.com>
2023-03-07 01:00:15 +0530

    Learning

---
+++ Parameters.java
@@ -0,0 +1,10 @@
+public class Parameters {
+
+    public static void main(String args[]){
+        System.out.println("The area of rectangle is "+ arearectangle(2.5,3.5));
+
+    }
+    public static double arearectangle(double length, double width){
+        return  length*width;
+    }
+}
